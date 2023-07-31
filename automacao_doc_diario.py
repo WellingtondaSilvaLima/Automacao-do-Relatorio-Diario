edc5a550177a7a28264f9e3be991d867ada88be2
Wellington Lima <wellingtonlima.jt@gmail.com>
2023-07-31 10:15:01 -0300

    Criei uma automação para o meu trabalho
    Uma automação simples em Python que facilita meu trabalho diário.

---
+++ automacao_doc_diario.py
@@ -0,0 +1,535 @@
+with open('instalacao.txt', 'a') as arquivo:
+    arquivo.write('''\n\n\n********************************************************************************************
+12/06/2023
+********************************************************************************************
+
+	1-
+____________________________________________________________________________________________
+
+	2-
+____________________________________________________________________________________________
+
+	3-
+____________________________________________________________________________________________
+
+	4-
+____________________________________________________________________________________________
+
+	5-
+____________________________________________________________________________________________
+
+	6-
+____________________________________________________________________________________________
+
+	7-
+____________________________________________________________________________________________
+
+	8-
+____________________________________________________________________________________________
+
+	9-
+____________________________________________________________________________________________
+
+	10-
+____________________________________________________________________________________________
+
+	11-
+____________________________________________________________________________________________
+
+	12-
+____________________________________________________________________________________________
+
+	13-
+____________________________________________________________________________________________
+
+	14-
+____________________________________________________________________________________________
+
+	15-
+____________________________________________________________________________________________
+
+	16-
+____________________________________________________________________________________________
+
+	17-
+____________________________________________________________________________________________
+
+	18-
+____________________________________________________________________________________________
+
+	19-
+____________________________________________________________________________________________
+
+	20-
+____________________________________________________________________________________________''')
+
+
+with open('atendimento.txt', 'a') as arquivo:
+    arquivo.write('''\n\n\n********************************************************************************************
+12/06/2023
+********************************************************************************************
+
+1-
+____________________________________________________________________________________________
+
+2-
+____________________________________________________________________________________________
+
+3-
+____________________________________________________________________________________________
+
+4-
+____________________________________________________________________________________________
+
+5-
+____________________________________________________________________________________________
+
+6-
+____________________________________________________________________________________________
+
+7-
+____________________________________________________________________________________________
+
+8-
+____________________________________________________________________________________________
+
+9-
+____________________________________________________________________________________________
+
+10-
+____________________________________________________________________________________________
+
+11-
+____________________________________________________________________________________________
+
+12-
+____________________________________________________________________________________________
+
+13-
+____________________________________________________________________________________________
+
+14-
+____________________________________________________________________________________________
+
+15-
+____________________________________________________________________________________________
+
+16-
+____________________________________________________________________________________________
+
+17-
+____________________________________________________________________________________________
+
+18-
+____________________________________________________________________________________________
+
+19-
+____________________________________________________________________________________________
+
+20-
+____________________________________________________________________________________________''')
+
+
+with open('geracao_online.txt', 'a') as arquivo:
+    arquivo.write('''\n\n\n\n********************************************************************************************
+
+00/00/0000
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+1-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+2-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+3-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+4-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+5-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+6-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+7-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+8-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+9-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+10-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+11-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+12-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+13-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+14-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+15-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+16-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+17-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+18-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+19-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+20-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+21-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+
+*** cliente com liberação para cadastro do SIM CLUB****
+
+22-
+
+	Telefone: 
+	Contato: 
+	Pacote: 
+	Licença: 
+	Login: 
+	Senha: 
+*** cliente com liberação para cadastro de cursos – ESCOLA MESTRE AUTOMOTIVO****
+
+[ ] Verde
+[ ] Vermelho
+
+Vendedor: 
+____________________________________________________________________________________________
+''')
+
+
